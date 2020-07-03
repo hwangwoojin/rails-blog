1324d7db0a24974857390823ee8d39e1f05d02ae
hwangwoojin <hwangwoojin.dev@gmail.com>
2020-07-03 15:48:37 +0900

    getting started end

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+    http_basic_authenticate_with name: "name", password: "password", except: [:index, :show]
+
     def index
         @articles = Article.all
     end
@@ -38,6 +41,7 @@ class ArticlesController < ApplicationController
     def destroy
         @article = Article.find(params[:id])
         @article.destroy
+        redirect_to action: :index
     end
        
     private
